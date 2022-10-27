b6a0efa0da22a826f140f66b61584840c0a6c639
yunut <somewheres@nate.com>
2022-10-27 20:22:27 +0900

    kotlin ans webflux mvc init

---
+++ src/main/kotlin/com/jys/webflux_ecommerce/WebfluxEcommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.jys.webflux_ecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxEcommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxEcommerceApplication>(*args)
+}
---
+++ src/test/kotlin/com/jys/webflux_ecommerce/WebfluxEcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jys.webflux_ecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxEcommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
