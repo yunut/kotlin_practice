8c42e51f62edc652920fca8ba590a5e7ba3d2e74
sangholim <55565835+sangholim@users.noreply.github.com>
2022-11-16 20:23:33 +0900

    의존성 라이브러리 및 테스트 환경 구축 (#7)
    * build.gradle.kts 수정
    
    * Test-Container Mongo 추가
    
    * KotestProjectConfig 추가
    
    * SampleTest 추가
    
    * application.yml: server.port 값 제거
    
    Co-authored-by: caramelsyrup <seojunlim87@gmail.com>

---
+++ src/test/kotlin/com/jys/kotlin_practice/config/DbExtension.kt
@@ -0,0 +1,21 @@
+package com.jys.kotlin_practice.config
+
+import io.kotest.core.annotation.AutoScan
+import io.kotest.core.listeners.AfterProjectListener
+import io.kotest.core.listeners.BeforeProjectListener
+import org.testcontainers.containers.MongoDBContainer
+
+/**
+ * Kotest Extension 기반 Test 전용 MongoDB Container 실행
+ *
+ * 참고: [Kotest > Extensions](https://kotest.io/docs/framework/extensions/extensions-introduction.html)
+ */
+@AutoScan
+object DbExtension : BeforeProjectListener, AfterProjectListener {
+    private val mongo = MongoDBContainer("mongo:4.0")
+
+    override suspend fun beforeProject() {
+        mongo.start()
+        System.setProperty("spring.data.mongodb.uri", mongo.replicaSetUrl)
+    }
+}
