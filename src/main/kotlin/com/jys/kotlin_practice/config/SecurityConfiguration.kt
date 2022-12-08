a11ab12cc0549bfd506a67b66616ec1c4008d8cb
yunut <somewheres@nate.com>
2022-12-08 21:28:28 +0900

    Spring Security opaque-token 검증 로직 추가

--- src/main/kotlin/com/jys/kotlin_practice/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/jys/kotlin_practice/config/SecurityConfiguration.kt
@@ -17,6 +17,10 @@ class SecurityConfiguration {
     @Bean
     fun webHttpSecurity(http: ServerHttpSecurity): SecurityWebFilterChain =
         http.csrf().disable().authorizeExchange {
-            it.pathMatchers("/**").permitAll()
-        }.build()
+            it.pathMatchers("/account/signin").permitAll()
+            it.pathMatchers("/account/signup").permitAll()
+            it.pathMatchers("/account/profile").permitAll()
+            it.pathMatchers("/css/**").permitAll()
+            it.pathMatchers("/js/**").permitAll()
+        }.oauth2ResourceServer { it.opaqueToken {  } }.build()
 }
